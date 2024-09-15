d3dd688d1244bc8d73daa029048b3f0b511a8aae
ryouol <royluo05@gmail.com>
2024-09-14 22:53:55 -0700

    Project set up

---
+++ shooting-stars/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StarField from './components/StarField';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <StarField />
+    </div>
+  );
+};
+
+export default App;
